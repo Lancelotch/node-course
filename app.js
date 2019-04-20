f9d49441d6ab1c1144157db439773b24829162f8
Candra Darmawan <chandra.souvenir@gmail.com>
2019-04-20 10:47:08 +0700

    Change writeFileAsync to writeFile instead because we don't want to hold excecution in system, and then we using callback

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 const http = require("http");
-const fs = require('fs'); //file System
+const fs = require("fs"); //file System
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.headers);
   const url = req.url;
   const method = req.method;
-  res.setHeader('Content-Type', 'text/html');
-  if(url === '/'){
-    res.write('<html>');
-    res.write('<head><title>Node Js course</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
-    res.write('</html>');
+  res.setHeader("Content-Type", "text/html");
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Node Js course</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
     return res.end();
   }
-  if(url === '/message' && method==='POST'){
+  if (url === "/message" && method === "POST") {
     const body = [];
-    req.on('data',(chunk)=> {
+    req.on("data", chunk => {
       console.log(chunk);
       body.push(chunk);
-    })
-    req.on('end', () => {
+    });
+    req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
       console.log(parseBody);
-      const message = parseBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    })
-    
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+      const message = parseBody.split("=")[1];
+      //Change writeFileAsync to writeFile instead because we don't want to hold excecution in system, and then we using callback
+      //This is Event driven code excecution architecture
+      return fs.writeFile("message.txt", message, error => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
-  res.write('<html>');
-  res.write('<head><title>Node Js course</title></head>');
-  res.write('<body><h1></h1></body>');
-  res.write('</html>');
+  res.write("<html>");
+  res.write("<head><title>Node Js course</title></head>");
+  res.write("<body><h1></h1></body>");
+  res.write("</html>");
   res.end();
 });
 
